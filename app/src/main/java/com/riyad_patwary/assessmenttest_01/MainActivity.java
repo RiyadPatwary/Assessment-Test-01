32292a3e13e95ce9fd09b1601ba9129757d7782d
RIYAD Patwary <riyadpatwary2012@gmail.com>
2019-12-23 11:36:18 +0600

    layout done

--- app/src/main/java/com/riyad_patwary/assessmenttest_01/MainActivity.java
+++ app/src/main/java/com/riyad_patwary/assessmenttest_01/MainActivity.java
@@ -1,42 +1,50 @@
 package com.riyad_patwary.assessmenttest_01;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.tomer.fadingtextview.FadingTextView;
+
 public class MainActivity extends AppCompatActivity {
     ImageView background_img, clover;
-    LinearLayout textSplash, textHome, menus;
+    LinearLayout textSplash;
+    LinearLayout textHome;
+    ConstraintLayout mainView;
     Animation fromBottom;
+    FadingTextView fadingTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //load animation...
-        fromBottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        fromBottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
 
         //layout mapping...
         background_img = (ImageView) findViewById(R.id.backGround_img_id);
         clover = (ImageView) findViewById(R.id.clover_id);
         textSplash = (LinearLayout) findViewById(R.id.textSplash_id);
         textHome = (LinearLayout) findViewById(R.id.textHome_id);
-        menus = (LinearLayout) findViewById(R.id.menus_id);
+        mainView = (ConstraintLayout) findViewById(R.id.mainView_constraint_id);
+        fadingTextView = (FadingTextView) findViewById(R.id.fadingTextView_id);
 
-        //animating...
-        background_img.animate().translationY(-2000).setDuration(1500).setStartDelay(300);
-        clover.animate().alpha(0).setDuration(1500).setStartDelay(300);
-        textSplash.animate().translationY(-650).alpha(0).setDuration(1000).setStartDelay(300);
-        textSplash.animate().translationX(-150).alpha(0).setDuration(1000).setStartDelay(300);
-        //menus.animate().translationY(-150).alpha(0).setDuration(5000).setStartDelay(300);
+        //animation customization...
+        background_img.animate().translationY(-2000).setDuration(500).setStartDelay(2500);
+        clover.animate().alpha(0).setDuration(2000).setStartDelay(300);
+        textSplash.animate().translationY(-650).alpha(0).setDuration(600).setStartDelay(2500);
+        textSplash.animate().translationX(-150).alpha(0).setDuration(600).setStartDelay(2500);
 
+        //start animation...
         textHome.startAnimation(fromBottom);
-        menus.startAnimation(fromBottom);
+        mainView.startAnimation(fromBottom);
 
 
     }
---
+++ app/src/main/java/com/riyad_patwary/assessmenttest_01/temp.java
@@ -0,0 +1,14 @@
+package com.riyad_patwary.assessmenttest_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class temp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_temp);
+    }
+}
